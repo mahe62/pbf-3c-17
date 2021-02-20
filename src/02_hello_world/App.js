62e05e37cfe51555bfe92143240250c7d6eff97a
Rizqi Mahendra <mahee6232@gmail.com>
2021-02-21 00:20:52 +0700

    prak2

---
+++ src/02_hello_world/App.js
@@ -0,0 +1,27 @@
+import logo from './mul.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Hallo Gaes</h1>
+        <p>
+          Muhammad Rizqi Mahendra - 
+          1841720144
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
