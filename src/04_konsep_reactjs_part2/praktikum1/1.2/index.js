087b6f1f2f9ce98b5d80a2aa9692ec9219ea29d2
Rizqi Mahendra <mahee6232@gmail.com>
2021-03-09 10:58:42 +0700

    prak1

---
+++ src/04_konsep_reactjs_part2/praktikum1/1.2/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+class Toggle extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isToggleOn: true };
+
+    // // binding ini dibutuhkan agar dapat bekerja ketika pemanggilan
+    // this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = () => {
+    this.setState(state => ({
+      isToggleOn: !state.isToggleOn
+    }));
+  }
+
+  render () {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isToggleOn ? 'ON - 184720144' : 'OFF - Muhammad Rizqi Mahendra'}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Toggle />,
+  document.getElementById('root')
+);
