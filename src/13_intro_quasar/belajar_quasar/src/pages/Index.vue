c427e14e89b6785c4af5c61c417608267692e752
mahe62 <mahee6232@gmail.com>
2021-06-19 00:13:27 +0700

    src

---
+++ src/13_intro_quasar/belajar_quasar/src/pages/Index.vue
@@ -0,0 +1,16 @@
+<template>
+  <q-page class="flex flex-center">
+    <h3>{{ hello }}</h3>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data() {
+    return {
+      hello: "Halo, Nama Saya Paralon dengan NIM 1841720144"
+    }
+  }
+}
+</script>
